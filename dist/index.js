774e49ee07880e860c964721f6964aa637ccef58
haha370104 <137368440@qq.com>
2017-03-10 10:48:11 +0800

    export

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+/**
+ * Created by haha370104 on 2017/3/10.
+ */
+
+import { startWatch } from 'dist/lib/imageWatcher';
+
+export const startFileWatch = startWatch;
+//# sourceMappingURL=index.js.map
--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 /**
  * Created by haha370104 on 2017/3/10.
  */
+
+import {startWatch} from 'dist/lib/imageWatcher'
+
+export const startFileWatch = startWatch;
